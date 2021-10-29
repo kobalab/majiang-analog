0f8bef859ce7873703e489ad2436765874a3cc3d
Satoshi Kobayashi <koba@yk.rim.or.jp>
2021-10-30 07:43:43 +0800

    examples/search.js を追加 - 和了役と流局理由の検索

---
+++ examples/search.js
@@ -0,0 +1,27 @@
+/*
+ *  和了役と流局理由の検索
+ */
+"use strict";
+
+class AnaLog extends require('../').base {
+
+    hule(hule) {
+        if (hule.hupai.find(h=> h.name == search))
+                                    console.log(this.idx(hule.l));
+    }
+    pingju(pingju) {
+        if (pingju.name == search)  console.log(this.idx());
+    }
+}
+
+const yargs = require('yargs');
+const argv = yargs
+    .usage('Usage: $0 <log-dir> <search>')
+    .option('times',     { alias: 't' })
+    .option('silent',    { alias: 's', boolean: true })
+    .demandCommand(2)
+    .argv;
+const filename = argv._[0];
+const search   = argv._[1];
+
+AnaLog.analyze(filename, argv);
