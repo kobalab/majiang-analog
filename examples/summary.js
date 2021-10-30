48025ac59906679998dd2c3cd90d60c0846f4fd4
Satoshi Kobayashi <koba@yk.rim.or.jp>
2021-10-30 23:12:07 +0800

    examples/summary.js の出力方法を変更

--- examples/summary.js
+++ examples/summary.js
@@ -22,10 +22,10 @@ class AnaLog extends require('../').base {
     kaiju(paipu) {
         this._result.n_game++;
         if (this._viewpoint != null) {
-            if (! this._result.rank) this._result.rank = [ 0, 0, 0, 0 ];
-            this._result.rank[paipu.rank[this._viewpoint] - 1]++;
-            if (! this._result.point) this._result.point = 0;
-            this._result.point += + paipu.point[this._viewpoint];
+            if (! this._result.n_rank) this._result.n_rank = [ 0, 0, 0, 0 ];
+            this._result.n_rank[paipu.rank[this._viewpoint] - 1]++;
+            if (! this._result.sum_point) this._result.sum_point = 0;
+            this._result.sum_point += + paipu.point[this._viewpoint];
         }
     }
     qipai(qipai) {
@@ -76,4 +76,64 @@ const argv = yargs
     .argv;
 const filename = argv._[0];
 
-console.log(AnaLog.analyze(filename, argv)._result);
+const r = AnaLog.analyze(filename, argv)._result;
+
+if (argv.player) {
+
+    function round(n, r) {
+        n = '' + n;
+        if (! n.match(/\./)) return n;
+        if (r == 0) return n.replace(/\..*$/,'');
+        for (let i = 0; i < r; i++) { n += '0' }
+        const regex = new RegExp(`(\\\.\\d{${r}}).*$`);
+        return n.replace(/^0\./,'.').replace(regex, '$1');
+    }
+
+    if (! argv.silent) console.log(r);
+
+    console.log(argv.player);
+    console.log('対局数: ' + r.n_game + ' (' + r.n_rank.join(' + ') + ')');
+    console.log(
+        '平均順位: '   + round([1,2,3,4].map(x=>r.n_rank[x-1]*x)
+                                      .reduce((x,y)=>x + y)
+                                / r.n_game, 2) + '、'
+        + 'トップ率: ' + round(r.n_rank[0] / r.n_game, 3) + '、'
+        + '連対率: '   + round((r.n_rank[0] + r.n_rank[1]) / r.n_game, 3) + '、'
+        + 'ラス率: '   + round(r.n_rank[3] / r.n_game, 3)
+    );
+    console.log(
+          '和了率: '   + round(r.n_hule / r.n_ju, 3) + '、'
+        + '放銃率: '   + round(r.n_bao / r.n_ju, 3) + '、'
+        + '立直率: '   + round(r.n_lizhi / r.n_ju, 3) + '、'
+        + '副露率: '   + round(r.n_fulou / r.n_ju, 3) + '、'
+        + '平均打点: ' + round(r.sum_defen / r.n_hule, 0)
+    );
+}
+else if (argv.viewpoint != null) {
+
+    console.log(
+        '========',
+        r.n_game,
+        r.n_ju,
+        r.n_rank.map(n => Math.round(n / r.n_game * 1000) / 1000),
+        Math.round((r.n_rank[0]*1 + r.n_rank[1]*2
+                    + r.n_rank[2]*3 + r.n_rank[3]*4)
+                                                / r.n_game * 100) /100,
+        Math.round(r.sum_point / r.n_game * 10) / 10,
+        '==',
+        Math.round(r.n_hule    / r.n_ju * 1000) / 1000,
+        Math.round(r.n_bao     / r.n_ju * 1000) / 1000,
+        Math.round(r.n_lizhi   / r.n_ju * 1000) / 1000,
+        Math.round(r.n_fulou   / r.n_ju * 1000) / 1000,
+        Math.round(r.sum_defen / r.n_hule),
+        '==',
+        r.n_hule, r.hule,
+        '==',
+        r.n_pingju,
+        Math.round(r.n_pingju / r.n_ju * 1000) / 1000,
+        r.pingju
+    );
+}
+else {
+    console.log(r);
+}
